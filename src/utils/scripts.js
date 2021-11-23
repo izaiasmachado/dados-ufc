f260d42488b0fc6502c385b58423246022233983
Matheus Rocha <monteiromatheus.rocha@gmail.com>
2021-11-22 23:09:18 -0300

    add: pergamum queries for reservations and pendencies

--- src/utils/scripts.js
+++ src/utils/scripts.js
@@ -1,9 +1,23 @@
+const FormData = require("form-data");
+
 const months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
 
-module.exports = { portugueseMonthsNumber }
+module.exports = { portugueseMonthsNumber, createFormData }
 
 function portugueseMonthsNumber (month) {
   for (let i = 0; i < 12; i++) {
     if (month.indexOf(months[i]) !== -1) return i
   }
+}
+
+function createFormData(flag,login,password,button,ifsp_categ){
+  const formData = new FormData();
+
+  formData.append('flag',flag);
+  formData.append('login',login);
+  formData.append('password',password);
+  formData.append('button',button);
+  formData.append('ifsp_categ',ifsp_categ);
+
+  return formData;
 }
